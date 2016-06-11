058519a79fa09135bf77c60f2449625cc9f04219
JongSikLim <whdtlr1218@gmail.com>
2016-06-11 19:07:27 +0900

    DB 구성 변경 Merge가 잘 되었는지 의문.. 등록후 페이지 복귀 구현

--- src/client/templates/enroll.js
+++ src/client/templates/enroll.js
@@ -10,24 +10,29 @@ import './enroll.html';
 
 Template.enroll.events({
     "submit .enroll-project":function (event) {
-
-//        event.preventDefault();
         const target = event.target;
         const title = target.title.value;
         const targetInvest = target.invest.value;
-
         const content = target.content.value;
-
         console.log(title);
-        console.log(invest);
+        console.log(targetInvest);
         console.log(content);
 
-        if(title==='' || invest==='' || content ===' '){
+        if(title==='' || targetInvest==='' || content ===' '){
             event.preventDefault();
             alert("모든 내용을 다 채우세요.");
             return;
         }
-        fundings.insert({owner: Meteor.userId(),username: Meteor.user().username ,title,content,targetInvest,currentInvest:"0", rate:"0",createdAt: new Date()});
+        fundings.insert({
+            owner: Meteor.userId(),
+            username: Meteor.user().username,
+            title,
+            content,
+            targetInvest,
+            currentInvest:"0",
+            rate:"0",
+            createdAt: new Date()
+        });
 
         console.log("뒤로가라");
         history.back();
